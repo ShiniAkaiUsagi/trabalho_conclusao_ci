9070f80b869b425c6a1a7fcf6687c44af639ff99
Pedro Dias <pedrovictor.dias@live.com>
2024-10-27 15:18:28 -0300

    LINT FIX: eslint reconfigured and project formatted

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+import {
+  beforeRunHook,
+  afterRunHook,
+} from "cypress-mochawesome-reporter/lib/index.js"; // Specify the index.js file directly
+
+export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
+
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
+    },
+  },
+});
